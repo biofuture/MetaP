2182e7ee6fb69dbf4425c6b9b21672cf016b2849
JIANG Xiaotao <biofuture.jiang@gmail.com>
2012-10-09 16:49:44 +0800

    Add a package to generate OtuTable

---
+++ 16S/OtuTable.pm
@@ -0,0 +1,12 @@
+package OtuTable.pm
+
+use strict;
+
+##pod
+=head1
+This package is used to merge the taxon information and the OTU information 
+to generate a OTU table for all samples.
+
+=cut
+
+
